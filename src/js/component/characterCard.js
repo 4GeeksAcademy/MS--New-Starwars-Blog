ee1b1b6c8c12ec4016d1354ef6feb4683b067bc6
monalisasingh87 <monalisa.singh87@gmail.com>
2024-09-23 13:44:40 +0000

    working on more

--- src/js/component/characterCard.js
+++ src/js/component/characterCard.js
@@ -6,33 +6,35 @@ import { Context } from "../store/appContext";
 
 
 export const CharacterCard = () => {
-	const { store, actions } = useContext(Context);
-    const [characters, setCharacters] = useState([]);
+  const { store, actions } = useContext(Context);
+  const [characters, setCharacters] = useState([]);
 
-    useEffect(() => {
+      useEffect(() => {
         async function fetchData() {
-            const res = await fetch("https://swapi.dev/api/people/");
-            const data = await res.json();
-            setCharacters(data.results);
+          const res = await fetch("https://swapi.dev/api/people/");
+          const data = await res.json();
+          setCharacters(data.results);
         }
         fetchData();
-    }, []);
-
+      }, []);
+    function handleFavorites(name) {
+      store.favorites.includes(name) ? actions.removeFromFavorites(name) : actions.addToFavorites(name);
+    }
 	return (
-		<div className="container d-flex col-10 overflow-auto mt-5 mx-auto">
-			{characters?.map((character, index) => (
-                <div className="card" style={{minWidth:"200px"}} key={index}>
-                    {console.log(character.name)}
-                <img src="..." className="card-img-top" alt="..."/>
-                <div className="card-body">
-                <h5 className="card-title text-dark">{character.name}</h5>
-
-                  <a href="#" className="btn btn-primary">Learn More</a>
-                </div>
-              </div>
-            )) }
-
-
+    <div className="container d-flex col-10 overflow-auto mt-5 mx-auto">
+      {characters?.map((character, index) => (
+        <div className="card" style={{ minWidth: "200px" }} key={index}>
+          {console.log(character.name)}
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title text-dark">{character.name}</h5>
+            <button className="btn btn-primary" onClick={() => handleFavorites(character.name)}>
+              <i className="far fa-heart"></i>
+            </button>
+            <Link to={"characterDescription/" + (index + 1)} className="btn btn-primary">Learn More</Link>
+          </div>
+        </div>
+		  )) }
 		</div>
 	);
 };
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,29 @@
 import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-dark bg-dark mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+	const { store, actions } = useContext(Context);
+	const [favorites, setFavorites] = useState([]);
+	// setFavorites(store.favorites);
+	useEffect(() => {
+		setFavorites(store.favorites);
+	}, [store.favorites]);
+		return (
+			<div className="container d-flex col-10 overflow-auto mt-5 mx-auto">
+			  {planets?.map((planet, index) => (
+				<div className="card" style={{ minWidth: "200px" }} key={index}>
+				  <img src="..." className="card-img-top" alt="..." />
+				  <div className="card-body">
+					<h5 className="card-title text-dark">{planet.name}</h5>
+					<button className="btn btn-primary" onClick={() => handleFavorites(planet.name)}>
+					  <i className="far fa-heart"></i>
+					</button>
+					<Link to={"planetDescription/" + (index + 1)} className="btn btn-primary">Learn More</Link>
+				  </div>
+				</div>
+			  ))}
 			</div>
-		</nav>
-	);
-};
+		  );
+		};
